b26b228f1cbdfb883e511d8e285485b8c4a3aa06
Jose Laruta <eduardo.laruta@gmail.com>
2024-10-23 08:12:22 -0400

    code agent2

--- 6.multiagent/6.4.code_agents/run_agent.py
+++ 6.multiagent/6.4.code_agents/run_agent.py
@@ -17,6 +17,13 @@ coding_agent = Agent(
     allow_code_execution=True,
     llm=llm,
 )
+review_agent = Agent(
+    role="Senior Python Developer",
+    goal="Perform thorough and concise code reviews.",
+    backstory="""You are a senior Python developer with extensive experience 
+    in software architecture, programming best practices and code reviews.""",
+    llm=llm,
+)
 
 generate_hello_task = Task(
     description="Generate a simple python program that receive the name of the user as input and prints a greeting with the name and a random integer between 1 and 10.",
@@ -29,7 +36,7 @@ generate_hello_task = Task(
 read_og_file_task = Task(
     description="Read the original program implementation.",
     expected_output="""The full original implementation in python with comments.""",
-    agent=coding_agent,
+    agent=review_agent,
     tools=[FileReadTool(file_path=os.path.join(current_dir, "hello.py"))],
 )
 
@@ -37,12 +44,12 @@ analyze_og_file_task = Task(
     description="Read the original program implementation and compare with the program generated.",
     expected_output="""A file with a detailed comparison and analysis of code practices between the 
     two files. Also make improvement recommendations for the original implementation.""",
-    agent=coding_agent,
+    agent=review_agent,
     output_file="analysis.md"
 )
 
 dev_crew = Crew(
-    agents=[coding_agent],
+    agents=[coding_agent, review_agent],
     tasks=[generate_hello_task, read_og_file_task, analyze_og_file_task],
     verbose=True
 )
